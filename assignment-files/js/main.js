67571d79f0845ae2bb65b36b3b8637ec74c57c07
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2018-11-28 18:59:47 -0600

    first commit

---
+++ assignment-files/js/main.js
@@ -0,0 +1,16 @@
+// GO! (check the 'TASK *' html comments for the relevant elements to manipulate )
+
+
+/* TASK 1 -- Show/Hide Nav */
+
+
+/* TASK 2 -- Select/Deslect an Icon */
+
+
+/* TASK 3 -- Increase total number*/
+
+
+/* TASK 4 (Adventure Mode)-- Move Item List to List */
+
+
+/* TASK 5 (Adventure Mode) -- Change Text Background Color From Palette */
