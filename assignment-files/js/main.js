94bb6f72d0824a9e0888a88739b915c54604de29
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2018-11-28 21:50:31 -0600

    first

--- assignment-files/js/main.js
+++ assignment-files/js/main.js
@@ -2,12 +2,41 @@
 
 
 /* TASK 1 -- Show/Hide Nav */
+document.querySelector(".btn-toggle-menu").addEventListener("click", function () {
+
+    if (document.querySelector(".answer-box nav").className === 'nav-menu') {
+        document.querySelector(".answer-box nav").className = "nav-menu-hidden";
+        document.querySelector(".btn-toggle-menu").innerHTML = "Show nav";
+
+    } else {
+        document.querySelector(".answer-box nav").className = "nav-menu";
+        document.querySelector(".btn-toggle-menu").innerHTML = "Hide nav";
+    }
+})
 
 
 /* TASK 2 -- Select/Deslect an Icon */
+document.querySelectorAll(".option").forEach(function (div) {
+    div.addEventListener('click', function (e) {
+        if (e.currentTarget.className === "option") {
+            e.currentTarget.classList.add('selected')
+        } else {
+            e.currentTarget.classList.remove('selected')
+        }
+
+    })
+})
 
 
 /* TASK 3 -- Increase total number*/
+document.querySelectorAll(".point").forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        var suma = document.querySelector(".total-points");
+        suma.textContent = parseInt(suma.textContent) + parseInt(btn.textContent)
+
+
+    })
+})
 
 
 /* TASK 4 (Adventure Mode)-- Move Item List to List */
